dfa92993b84954a5fb58e701584887b0e2c373fb
cydia2020 <12470297+cydia2020@users.noreply.github.com>
2022-09-05 23:09:39 +1000

    Rocket league model (commaai#24869)
    Co-Authored-By: HaraldSchafer <harald.the.engineer@gmail.com>
    Co-Authored-By: YassineYousfi <29985433+YassineYousfi@users.noreply.github.com>

--- selfdrive/controls/plannerd.py
+++ selfdrive/controls/plannerd.py
@@ -17,7 +17,7 @@ def plannerd_thread(sm=None, pm=None):
   CP = car.CarParams.from_bytes(params.get("CarParams", block=True))
   cloudlog.info("plannerd got CarParams: %s", CP.carName)
 
-  use_lanelines = True
+  use_lanelines = False
   wide_camera = params.get_bool('EnableWideCamera') if TICI else False
 
   cloudlog.event("e2e mode", on=use_lanelines)
