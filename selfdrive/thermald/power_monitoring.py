17ee4769f43b48f607b12ce7b3117b4b5723f2f2
Sumit Basak <basaksum@umich.edu>
2022-09-06 12:36:25 -0400

    Increase offroad time slightly

--- selfdrive/thermald/power_monitoring.py
+++ selfdrive/thermald/power_monitoring.py
@@ -20,8 +20,8 @@ CAR_CHARGING_RATE_W = 45
 
 VBATT_PAUSE_CHARGING = 11.0           # Lower limit on the LPF car battery voltage
 VBATT_INSTANT_PAUSE_CHARGING = 7.0    # Lower limit on the instant car battery voltage measurements to avoid triggering on instant power loss
-MAX_TIME_OFFROAD_S = 0.5*3600
-MIN_ON_TIME_S = 900
+MAX_TIME_OFFROAD_S = 3*3600
+MIN_ON_TIME_S = 1800
 
 class PowerMonitoring:
   def __init__(self):
